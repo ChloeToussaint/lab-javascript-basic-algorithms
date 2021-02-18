40494789fa1605ba1b8e6bc2d4256242b30567e9
chloe toussaint <toussaintchloe@hotmail.fr>
2021-02-18 21:12:11 +0100

    iteration une done

--- js/index.js
+++ js/index.js
@@ -1,5 +1,9 @@
 // Iteration 1: Names and Input
+const hacker1 = "Paul"
+console.log (`The driver's name is ${hacker1}`)
 
+const hacker2 = "Christophe"
+console.log (`The navigator's name is ${hacker2}`)
 
 // Iteration 2: Conditionals
 
