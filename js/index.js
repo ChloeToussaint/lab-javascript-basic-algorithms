1f6eb1ddc157828000fa237ab6ad7f1ab622928d
chloe toussaint <toussaintchloe@hotmail.fr>
2021-02-19 18:48:00 +0100

    exo terminé

--- js/index.js
+++ js/index.js
@@ -6,6 +6,11 @@ const hacker2 = "Christophe"
 console.log (`The navigator's name is ${hacker2}`)
 
 // Iteration 2: Conditionals
-
-
+if (hacker1  <5 ){
+console.log ('The driver has the longest name, it has 4 characters')
+}
+if else (hacker2 > 5){
+console.log ('It seems that the navigator has the longest name, it has 9 characters')
+}
+else {Wow, you both have equally long names, XX characters!}
 // Iteration 3: Loops
